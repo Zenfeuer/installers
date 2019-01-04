af49a92e1341564fe5b6effa6443756790e7def7
Darwing Hernandez <darwing.her69@gmail.com>
2019-01-03 21:17:59 -0800

    Tabs -> Spaces

--- web.sh
+++ web.sh
@@ -10,85 +10,85 @@ RESET="\e[0m"
 
 echo -e "${CYAN}Web Environment Installer v0.1${RESET}\n"
 
-SERVER="apache2"		# Default apache2
-DATABASE="mysql"		# Default mysql
-FRAMEWORK="laravel"		# Default laravel
-PHPV="7.2"				# Default PHP version
-VERBOSE=false			# Default false
-RESPONSE=				# User response
+SERVER="apache2"        # Default apache2
+DATABASE="mysql"        # Default mysql
+FRAMEWORK="laravel"     # Default laravel
+PHPV="7.2"              # Default PHP version
+VERBOSE=false           # Default false
+RESPONSE=               # User response
 
 # if first parameter is help, show help doc and ignore the following given commands
 if [[ $1 == "--help" || $1 == "-h" ]]; then
-	# TODO: Write helping doc
-	echo "Helping"
+    # TODO: Write helping doc
+    echo "Helping"
 elif [[ 1 ]]; then
 
-	# Using default preset parameters and notify it
-	if [[ $# -eq 0 ]]; then
+    # Using default preset parameters and notify it
+    if [[ $# -eq 0 ]]; then
 
-		echo -en "${BYELLOW}Warning:${RESET}"
-		echo -e "${YELLOW} Working with default parameters.${RESET}"
+        echo -en "${BYELLOW}Warning:${RESET}"
+        echo -e "${YELLOW} Working with default parameters.${RESET}"
 
-	elif [[ 1 ]]; then
-		
-		while [ "$1" != "" ]; do
-		    case $1 in -s | -S | --server )
+    elif [[ 1 ]]; then
+        
+        while [ "$1" != "" ]; do
+            case $1 in -s | -S | --server )
 
-				shift
-				SERVER=$1
-				;;
+                shift
+                SERVER=$1
+                ;;
 
-		        -d | -D | --database )
+                -d | -D | --database )
 
-				shift
-				DATABASE=$1
-				;;
+                shift
+                DATABASE=$1
+                ;;
 
-		        -f | -F | --framework )
-			
-				shift
-				FRAMEWORK=$1
-				;;
+                -f | -F | --framework )
+            
+                shift
+                FRAMEWORK=$1
+                ;;
 
-				-pv | -PV | --phpv )
-			
-				shift
-				PHPV=$1
-				;;
+                -pv | -PV | --phpv )
+            
+                shift
+                PHPV=$1
+                ;;
 
-				-v | -V | --verbose )
-			
-				VERBOSE=true
-				;;
+                -v | -V | --verbose )
+            
+                VERBOSE=true
+                ;;
 
-		        * ) 
-				
-				echo -e "${RED}Wrong parameter given: ${RESET}$1"
-				exit 1
-		    esac
-		    shift
-		done
+                * ) 
+                
+                echo -e "${RED}Wrong parameter given: ${RESET}$1"
+                exit 1
+            esac
+            shift
+        done
 
-	fi
+    fi
 
-	echo -e "${GREEN}The following packages are going to be installed:"
-	echo -en "${LGREEN}"
-	echo -e "  * $SERVER"
-	echo -e "  * $DATABASE"
-	echo -e "  * PHP v${PHPV}"
-	echo -e "  * $FRAMEWORK"
-	echo -e "${RESET}"
+    echo -e "${GREEN}The following packages are going to be installed:"
+    echo -en "${LGREEN}"
+    echo -e "  * $SERVER"
+    echo -e "  * $DATABASE"
+    echo -e "  * PHP v${PHPV}"
+    echo -e "  * $FRAMEWORK"
+    echo -e "${RESET}"
 
-	echo -en "${YELLOW}Confirm installation?${RESET} (Y/N) >"
+    echo -en "${YELLOW}Confirm installation?${RESET} (Y/N) >"
 
-	read RESPONSE
+    read RESPONSE
 
-	if [[ $RESPONSE != "Y" ]]; then
-		echo -e "${LYELLOW}Nothing was installed.${RESET}"
-		echo -e "${CYAN}Bye!${RESET}"
-		exit 1
-	else
-		echo "YES!!!!"
-	fi
+    if [[ $RESPONSE != "Y" ]]; then
+        echo -e "${LYELLOW}Nothing was installed.${RESET}"
+        echo -e "${CYAN}Bye!${RESET}"
+        exit 1
+    else
+        echo "YES!!!!"
+    fi
 
 fi
