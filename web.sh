087bb6590be3f6ea05f18051f361aa51a87b6ebf
Darwing Hernandez <darwing.her69@gmail.com>
2019-01-06 01:22:09 -0600

    Completing packages installation

--- web.sh
+++ web.sh
@@ -92,11 +92,16 @@ install_server()
     if [[ $SERVER == "apache2" ]]; then
         
         sudo apt install apache2 -y
-        sudo systemctl stop apache2.service -y
-        sudo systemctl start apache2.service -y
-        sudo systemctl enable apache2.service -y
+        sudo systemctl stop apache2.service
+        sudo systemctl start apache2.service
+        sudo systemctl enable apache2.service
 
-    #elif [[ $SERVER == "nginx" ]]; then
+    elif [[ $SERVER == "nginx" ]]; then
+
+        sudo apt install nginx -y
+        sudo systemctl stop nginx.service
+        sudo systemctl start nginx.service
+        sudo systemctl enable nginx.service
 
     fi
 }
